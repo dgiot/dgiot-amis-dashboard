2db3b1e8e44ad0fd4b750f0a2d3154b58a40c7cd
xuxiaobo1010 <73056200+xuxiaobo1010@users.noreply.github.com>
2022-04-01 10:00:18 +0000

    Deploying to gh-pages from  @ 3a0b3055c220ab76858329d0cbfddedf89b122cf 🚀

--- amis-00-tmp-schema.8de9728732667e16ffac.chunk.js
+++ amis-00-tmp-schema.8de9728732667e16ffac.chunk.js
@@ -5,6 +5,6 @@
  *  version : 0.0.2 
  *  description : dgiot-amis-dashboard 脚手架 
  *  author: h7ml(h7ml@qq.com) 
- *  time:Fri Apr 01 2022 09:26:45 GMT+0000 (Coordinated Universal Time)
+ *  time:Fri Apr 01 2022 09:57:42 GMT+0000 (Coordinated Universal Time)
  */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[5],{VZWQ:function(o,n,a){"use strict";a.r(n),a.d(n,"schema",(function(){return e}));var e={type:"page",body:{actionType:"dialog",dialog:{body:{}}}}}}]);
