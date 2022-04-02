e0f31f0f9483cb0744bfecf0aa5535f8229df54c
h7ml <dev@h7ml.cn>
2022-04-02 07:11:09 +0000

    Deploying to gh-pages from  @ cb67d3f818d1d242e5e6125d196849caccb843b5 🚀

--- amis-00-tmp-schema.8de9728732667e16ffac.chunk.js
+++ amis-00-tmp-schema.8de9728732667e16ffac.chunk.js
@@ -5,6 +5,6 @@
  *  version : 0.0.2 
  *  description : dgiot-amis-dashboard 脚手架 
  *  author: h7ml(h7ml@qq.com) 
- *  time:Sat Apr 02 2022 04:31:54 GMT+0000 (Coordinated Universal Time)
+ *  time:Sat Apr 02 2022 07:07:03 GMT+0000 (Coordinated Universal Time)
  */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[5],{VZWQ:function(o,n,a){"use strict";a.r(n),a.d(n,"schema",(function(){return e}));var e={type:"page",body:{actionType:"dialog",dialog:{body:{}}}}}}]);
